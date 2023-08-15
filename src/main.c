ae89fbdb48152ddbff559077af8829e71d64b697
Martin <mrt.thonon@gmail.com>
2023-08-15 15:42:59 +0200

    REFACTOR test

--- src/main.c
+++ src/main.c
@@ -12,31 +12,44 @@
 
 #include "minishell.h"
 
-int	main(int argc, char **argv, char **envp)
-{
-	t_ms	s_ms;
-	char	*text;
 
-	(void)argc;
-	(void)argv;
-	(void)envp;
-	s_ms.env = ft_dptrcpy(envp);
-    for (int i = 0; s_ms.env[i]; ++i) {
-        printf("->%s\n", s_ms.env[i]);
+
+int main() {
+
+    //const char *str = "get_replaced";
+    // Initialize Readline history
+    using_history();
+
+    char *input = readline("Enter a command: ");
+    while (input != NULL && *input != '\0') {
+        // Add the entered command to the history
+        add_history(input);
+
+        // Print the commands from the history
+        for (int i = history_length; i >= 0; i--) {
+            HIST_ENTRY *entry = history_get(i);
+            if (entry != NULL) {
+                printf("\nHistory[%d]: %s\n\n", i, entry->line);
+            }
+        }
+
+        // Free the input buffer
+        free(input);
+
+        // Get the next input
+        input = readline("Enter a command: ");
+        rl_on_new_line();
+        rl_replace_line("v", 0);
+        rl_redisplay();
+        printf("\n");
+        //fprintf(stdout, "-->%s\n", rl_line_buffer);
+
     }
 
-    printf("-->%s\n", ft_getenv("PATH", s_ms.env));
-	text = readline(PROMPT);
-	while (text != NULL)
-	{
-		if (*text != '\0')
-			add_history(text);
-		if (*text != '\0')
-			printf("%s\n", text);
-		free(text);
-		text = readline(PROMPT);
-	}
-	free(text);
-	rl_clear_history();
-	return (0);
+    // Clean up history and input
+    free(input);
+    rl_clear_history();
+    return 0;
 }
+
+
