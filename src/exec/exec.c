59cfa7a02719b3291cae70f173221a2dc284f472
shinfray <simon@hinfray.eu>
2023-10-20 15:27:50 +0200

    add comment for test

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -10,11 +10,19 @@ int			ft_exec_line(t_msh *msh, t_pipeline *pipeline);
 static int	ft_init_exl(t_exl *exl, t_msh *msh, t_pipeline *pipeline);
 static int	ft_exec_cmd(t_exl *exl, t_pipeline *pipeline);
 static int	ft_wait(pid_t last_pid);
-
+#include "utils.h"
 int	ft_exec_line(t_msh *msh, t_pipeline *pipeline)
 {
 	t_exl	s_exl;
 	int		exit_code;
+
+	// pipeline->cmd_list = ft_calloc(1, sizeof(t_cmd));
+	// pipeline->n_cmd = 1;
+	// pipeline->cmd_list->n_redirect = 1;
+	// pipeline->cmd_list->args = ft_split("cat", ' ');
+	// pipeline->cmd_list->redirect_arr = ft_calloc(1, sizeof(t_redirect));
+	// pipeline->cmd_list->redirect_arr->file = ft_strdup("EOF");
+	// pipeline->cmd_list->redirect_arr->e_iotype = HEREDOC;
 
 	if (ft_init_exl(&s_exl, msh, pipeline) == -1)
 	{
