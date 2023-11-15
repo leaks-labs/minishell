b0104d0079c9930fadec6b88cb873a2eeb4d5a3e
Simon Hinfray <108767313+shinfray@users.noreply.github.com>
2023-11-15 14:45:47 +0100

    FIX norm

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -46,18 +46,18 @@ static int	ft_init_exl(t_exl *exl, t_msh *msh, t_pl *pl)
 static int	ft_exec_cmd(t_msh *msh, t_exl *exl, t_pl *pl)
 {
 	t_built_f		built_f;
-	t_in_shell_pmt	pmt;
+	t_in_shell_pmt	s_pmt;
 
 	built_f = NULL;
-	pmt.msh = msh;
-	pmt.pl = pl;
-	pmt.exl = exl;
+	s_pmt.msh = msh;
+	s_pmt.pl = pl;
+	s_pmt.exl = exl;
 	if (pl->n_cmd == 1)
 	{
 		if (pl->cmd_list->args != NULL)
 			built_f = ft_get_builtin(*pl->cmd_list->args);
 		if (pl->cmd_list->args == NULL || built_f != NULL)
-			return (ft_in_shell(&pmt, pl->cmd_list, built_f));
+			return (ft_in_shell(&s_pmt, pl->cmd_list, built_f));
 	}
 	return (ft_in_subshell(msh, exl, pl));
 }
