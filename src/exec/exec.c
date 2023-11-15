3c0c251953a3d3bf62e4738e0151da8ce95bf72b
Simon Hinfray <108767313+shinfray@users.noreply.github.com>
2023-11-15 17:16:46 +0100

    add update_last_arg

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -1,11 +1,15 @@
 #include "exec.h"
+#include "env.h"
 #include "heredoc.h"
 #include "init.h"
 #include "msh_signal.h"
+#include "utils.h"
 
-uint8_t		ft_exec_line(t_msh *msh, t_pl *pl);
-static int	ft_init_exl(t_exl *exl, t_msh *msh, t_pl *pl);
-static int	ft_exec_cmd(t_msh *msh, t_exl *exl, t_pl *pl);
+uint8_t				ft_exec_line(t_msh *msh, t_pl *pl);
+static int			ft_init_exl(t_exl *exl, t_msh *msh, t_pl *pl);
+static int			ft_exec_cmd(t_msh *msh, t_exl *exl, t_pl *pl);
+static void			ft_update_last_arg(t_list *env, t_pl *pl);
+static const char	*ft_get_last_arg(const char **args);
 
 // #include "utils.h"
 // #include <unistd.h>
@@ -29,7 +33,7 @@ uint8_t	ft_exec_line(t_msh *msh, t_pl *pl)
 		return (1);
 	}
 	exit_code = ft_exec_cmd(msh, &s_exl, pl);
-	// update $_
+	ft_update_last_arg(&msh->env, pl);
 	ft_free_cmd_list(pl);
 	return ((uint8_t)exit_code);
 }
@@ -61,4 +65,22 @@ static int	ft_exec_cmd(t_msh *msh, t_exl *exl, t_pl *pl)
 			return (ft_in_shell(&pmt, pl->cmd_list, built_f));
 	}
 	return (ft_in_subshell(msh, exl, pl));
+}
+
+static void	ft_update_last_arg(t_list *env, t_pl *pl)
+{
+	const char	*last_arg;
+
+	if (pl->n_cmd > 1)
+		return ;
+	last_arg = ft_get_last_arg((const char **)pl->cmd_list->args);
+	ft_mod_env2(env, "_", last_arg, ENV_ASSIGN | ENV_NOEXP);
+	// check error mod_env2 or not?
+}
+
+static const char	*ft_get_last_arg(const char **args)
+{
+	if (args == NULL)
+		return (NULL);
+	return (args[ft_dptrlen(args) - 1]);
 }
