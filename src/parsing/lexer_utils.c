d2e9c9c606cb3a0fe606e6c08ae35a2fcdc8f37b
martinthonon <mrt.thonon@gmail.com>
2023-11-22 12:29:25 +0100

    update

--- src/parsing/lexer_utils.c
+++ src/parsing/lexer_utils.c
@@ -2,51 +2,54 @@
 #include "utils.h"
 #include <stdlib.h>
 
-t_lexer ft_get_token(t_token_container *token_container, char *line, t_lexer_operator operator, t_index *index);
-t_lexer ft_skip_quotes(char *line, t_index *index);
-void    ft_get_next_index(char *line, t_index *index);
+t_lexer	ft_get_token(t_token_container *token_container, char *line, \
+t_lexer_operator operator, t_index *index);
+t_lexer	ft_skip_quotes(char *line, t_index *index);
+void	ft_get_next_index(char *line, t_index *index);
 
-t_lexer ft_get_token(t_token_container *token_container, char *line, t_lexer_operator operator, t_index *index)
+t_lexer	ft_get_token(t_token_container *token_container, char *line, \
+t_lexer_operator operator, t_index *index)
 {
-    t_token *token;
+	t_token	*token;
 
-    token = ft_tokenise(operator, line, index);
-    if (token == NULL || ft_append(token_container, token) == NULL)
+	token = ft_tokenise(operator, line, index);
+	if (token == NULL || ft_append(token_container, token) == NULL)
 	{
 		free(token);
-        return (NOT_LEXED);
+		return (NOT_LEXED);
 	}
-    while (ft_isspace(line[index->current]) == true)
-        index->current++;
-    index->previous = index->current;
-    return (LEXED);
+	while (ft_isspace(line[index->current]) == true)
+		index->current++;
+	index->previous = index->current;
+	return (LEXED);
 }
 
-t_lexer ft_skip_quotes(char *line, t_index *index)
+t_lexer	ft_skip_quotes(char *line, t_index *index)
 {
-    const char tmp = line[index->current];
-    if (index->current != 0 && ft_isspace(line[index->current - 1]) == true)
-        index->previous = index->current;
-    index->current++;
-    while (line[index->current] != tmp && line[index->current] != '\0')
-        index->current++;
-    if (line[index->current] == '\0')
-        return (NOT_LEXED);
-    index->current++;
-    return (LEXED);
+	const char	tmp = line[index->current];
+
+	if (index->current != 0 && ft_isspace(line[index->current - 1]) == true)
+		index->previous = index->current;
+	index->current++;
+	while (line[index->current] != tmp && line[index->current] != '\0')
+		index->current++;
+	if (line[index->current] == '\0')
+		return (NOT_LEXED);
+	index->current++;
+	return (LEXED);
 }
 
-void ft_get_next_index(char *line, t_index *index)
+void	ft_get_next_index(char *line, t_index *index)
 {
-    t_dictionary dictionary;
+	t_dictionary	dictionary;
 
-    if (index->current == 0)
-    {
-        while (ft_isspace(line[index->current]) == true)
-            index->current++;
-        index->previous = index->current;
-    }
-    dictionary = ft_search_operator(&line[index->current]);
-    if (dictionary.operator_type == NO_OPERATOR && line[index->current] != '\0')
-        index->current++;
+	if (index->current == 0)
+	{
+		while (ft_isspace(line[index->current]) == true)
+			index->current++;
+		index->previous = index->current;
+	}
+	dictionary = ft_search_operator(&line[index->current]);
+	if (dictionary.operator_type == NO_OPERATOR && line[index->current] != '\0')
+		index->current++;
 }
