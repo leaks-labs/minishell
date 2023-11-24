e6566e979173477fadb8bc8f3896b81dce04aa8c
Simon Hinfray <108767313+shinfray@users.noreply.github.com>
2023-11-24 19:06:57 +0100

    fix heredoc

--- src/heredoc/retrieve_hd_content.c
+++ src/heredoc/retrieve_hd_content.c
@@ -6,7 +6,7 @@
 /*   By: Leex-Labs <leex-labs@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 14:55:36 by Leex-Labs         #+#    #+#             */
-/*   Updated: 2023/11/24 16:27:20 by Leex-Labs        ###   ########.fr       */
+/*   Updated: 2023/11/24 19:02:44 by Leex-Labs        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 #include <readline/readline.h>
 
 char		*ft_get_hd_content(t_msh *msh, char *del, unsigned int *line_num);
-static char	*ft_retrieve_one_line(t_msh *msh, const bool to_expand);
+static char	*ft_retrieve_one_line(void);
 static bool	ft_end_of_hd(char *current_line, char *del, unsigned int line_num);
 static char	*ft_update_hd_content(char *hd_content, char *current_line);
 
@@ -30,44 +30,44 @@ char	*ft_get_hd_content(t_msh *msh, char *del, unsigned int *line_num)
 	const unsigned int	first_line_num = *line_num;	
 	char				*hd_content;
 	char				*current_line;
+	char				*tmp;
 
 	if (to_expand == false)
 		ft_rm_quotes(del);
 	hd_content = ft_calloc(1, sizeof(char));
 	if (hd_content == NULL)
 		return (NULL);
-	current_line = ft_retrieve_one_line(msh, to_expand);
+	current_line = ft_retrieve_one_line();
 	if (g_signal_value > 0)
 		return (ft_freef("%p", hd_content));
 	while (ft_end_of_hd(current_line, del, first_line_num) == false)
 	{
+		if (current_line != NULL && to_expand == true)
+		{
+			tmp = current_line;
+			current_line = ft_expansion_monitor(msh, current_line, false);
+			free(tmp);
+		}
 		hd_content = ft_update_hd_content(hd_content, current_line);
 		if (hd_content == NULL)
 			break ;
 		(*line_num)++;
-		current_line = ft_retrieve_one_line(msh, to_expand);
+		current_line = ft_retrieve_one_line();
 		if (g_signal_value > 0)
 			return (ft_freef("%p", hd_content));
 	}
 	return (hd_content);
 }
 
-static char	*ft_retrieve_one_line(t_msh *msh, const bool to_expand)
+static char	*ft_retrieve_one_line(void)
 {
 	char	*current_line;
-	char	*tmp;
 
 	ft_set_signals(MSH_SIG_HEREDOC);
 	current_line = readline(HD_PROMPT);
 	ft_set_signals(MSH_SIG_IGN);
 	if (g_signal_value > 0 && current_line != NULL)
 		current_line = ft_freef("%p", current_line);
-	if (current_line != NULL && to_expand == true)
-	{
-		tmp = current_line;
-		current_line = ft_expansion_monitor(msh, current_line, false);
-		free(tmp);
-	}
 	return (current_line);
 }
 
